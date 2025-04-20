2645367cc82a75d9dd180d740ff0c3a8f1494f55
DTA32 <97021335+DTA32@users.noreply.github.com>
2025-04-21 00:05:04 +0700

    feat: disable awayfl due to type strict issue

--- src/app/components/GamePlayer.tsx
+++ src/app/components/GamePlayer.tsx
@@ -1,41 +1,17 @@
 import Game from "../interfaces/Game";
 import Script from "next/script";
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "awayfl-player": any;
-    }
-  }
-}
-
 export default function GamePlayer({ game }: { game: Game }) {
-  const src = (nameID: string) => {
-    return `${process.env.NEXT_PUBLIC_STATIC_URL}/games/${nameID}.swf`;
-  };
-  var emulatorScript: string = "";
-  var embedJSX: JSX.Element = <></>;
-  if (game.type === 1) {
-    emulatorScript = "/awayfl/embed.js";
-    embedJSX = (
-      <awayfl-player runtimebaseurl="/awayfl" src={src(game.nameID)} />
-    );
-  } else if (game.type === 2) {
-    emulatorScript = "https://unpkg.com/@ruffle-rs/ruffle";
-    embedJSX = (
-      <embed
-        src={src(game.nameID)}
-        width={game.dimension.width}
-        height={game.dimension.height}
-      />
-    );
-  }
-
+  
   return (
     <>
-      <Script src={emulatorScript} />
+      <Script src="https://unpkg.com/@ruffle-rs/ruffle" />
       <div className="flex justify-center align-center w-full h-full">
-        {embedJSX}
+        <embed
+          src={`${process.env.NEXT_PUBLIC_STATIC_URL}/games/${game.nameID}.swf`}
+          width={game.dimension.width}
+          height={game.dimension.height}
+        />
       </div>
     </>
   );
